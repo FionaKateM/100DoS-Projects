6b8359fe59230e6e1ab755c77a9d2d661bfc1795
Fiona Kate Morgan <fkmorgan@me.com>
2019-04-07 12:21:37 +0100

    End of day 52

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Fiona Kate Morgan on 07/04/2019.
+//  Copyright © 2019 Fiona Kate Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    var currentImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
+    
+}
+
---
+++ Project13/README.md
@@ -0,0 +1,14 @@
+# 100DoS-project13
+
+Following along with 100 Days of Swift by HackingWithSwift.com https://www.hackingwithswift.com/100
+
+This is days 52 - 54 and is an image filtering app.
+
+Skills include:
+- Saving edited images to the user's photos
+- UISlider
+- UIImagePickerController
+- Privacy in info.plist
+
+Challenges completed with my own code are:
+- tbc
